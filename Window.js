bbfb0948568d02d2ed15d1554747a8ac948355a9
Wiibleyde <nathan@bonnell.fr>
2023-11-08 10:44:13 +0100

    ça marche pas de ouf
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ Window.js
@@ -0,0 +1,25 @@
+const { BrowserWindow } = require('electron')
+
+const defaultProps = {
+    width: 500,
+    height: 800,
+    show: false,
+    
+    webPreferences: {
+        nodeIntegration: true
+    }
+}
+
+class Window extends BrowserWindow {
+    constructor ({ file, ...windowSettings }) {
+        super({ ...defaultProps, ...windowSettings })
+
+        this.loadFile(file)
+        
+        this.once('ready-to-show', () => {
+            this.show()
+        })
+    }
+}
+
+module.exports = Window
